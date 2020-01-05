990a0b4ef71a09b5052bceae6db22dc6341a64f0
galkeidarrr <39056499+galkeidarrr@users.noreply.github.com>
2020-01-05 16:23:59 +0200

    request unsplash API

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    //request for unsplash API to send us JSON with all the diffrent photos that he found
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID b20eacb5ce97108d4c87e3d53e77606a351b9b316a6986d6a9f9c2380d337cd9"
+      }
+    });
   }
   render() {
     return (
