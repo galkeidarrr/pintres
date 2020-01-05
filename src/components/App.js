38082589902e431e4d90cb55d31475a0e57872da
galkeidarrr <39056499+galkeidarrr@users.noreply.github.com>
2020-01-05 16:39:51 +0200

    response from API

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,25 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
+  state = { photos: [] };
+
+  onSearchSubmit = async term => {
     //request for unsplash API to send us JSON with all the diffrent photos that he found
-    axios.get("https://api.unsplash.com/search/photos", {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
         Authorization:
           "Client-ID b20eacb5ce97108d4c87e3d53e77606a351b9b316a6986d6a9f9c2380d337cd9"
       }
     });
-  }
+
+    this.setState({ photos: response.data.results });
+  };
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        found: {this.state.photos.length} photos
       </div>
     );
   }
