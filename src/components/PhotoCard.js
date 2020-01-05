e73abed178fa19620d5e1b6e0b57e4b624dc06a5
galkeidarrr <39056499+galkeidarrr@users.noreply.github.com>
2020-01-05 18:15:12 +0200

    photo styles

--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -1,12 +1,31 @@
 import React from "react";
 
 class PhotoCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { spans: 0 };
+    this.photoRef = React.createRef();
+  }
+
+  componentDidMount() {
+    //the photo doesnt loaded yet
+    this.photoRef.current.addEventListener("load", this.setSpans);
+  }
+
+  setSpans = () => {
+    const height = this.photoRef.current.clientHeight;
+    const spans = Math.ceil(height / 10);
+    this.setState({ spans });
+  };
+
   render() {
     const { descriptioin, urls } = this.props.photo;
     return (
-      <div>
-        <img alt={descriptioin} src={urls.regular} />
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.photoRef} alt={descriptioin} src={urls.regular} />
       </div>
     );
   }
 }
+
+export default PhotoCard;
